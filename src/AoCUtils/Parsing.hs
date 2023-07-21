c749f509d823c9445e796fe0784c52e8b333801b
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-07-21 18:05:43 +0200

    More general parsing

--- src/AoCUtils/Parsing.hs
+++ src/AoCUtils/Parsing.hs
@@ -1,11 +1,33 @@
-module AoCUtils.Parsing (numberParser) where
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
+module AoCUtils.Parsing (
+  parseUnsignedInts,
+  parseSignedInts
+) where
 
-import           Text.Parsec (Parsec, char, digit, many1, optionMaybe)
+import           Text.Parsec (Parsec, anyToken, char, digit, many1, optionMaybe,
+                              parse, sepBy)
 
-numberParser :: Parsec String () Integer
-numberParser = do
+parseUnsignedInts :: (Num a, Read a) => String -> Maybe [a]
+parseUnsignedInts = parseMaybe (manyParser unsignedIntegerParser)
+
+parseSignedInts :: (Num a, Read a) => String -> Maybe [a]
+parseSignedInts = parseMaybe (manyParser signedIntegerParser)
+
+parseMaybe :: Parsec String () a -> String -> Maybe a
+parseMaybe p str = case parse p "" str of
+  Right res -> Just res
+  Left _    -> Nothing
+
+manyParser :: Parsec String () a -> Parsec String () [a]
+manyParser p = sepBy p anyToken
+
+unsignedIntegerParser :: (Num a, Read a) => Parsec String () a
+unsignedIntegerParser = read <$> many1 digit
+
+signedIntegerParser :: (Num a, Read a) => Parsec String () a
+signedIntegerParser = do
   sign <- optionMaybe (char '-')
-  num <- read <$> many1 digit
+  num <- unsignedIntegerParser
   return $ case sign of
     Nothing -> num
     Just _  -> (-num)
