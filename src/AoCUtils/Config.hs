50e123d2c32e5a15d20e9ae23caff9d5a71de4dd
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2026-03-01 11:47:46 +0100

    Revert "Remove visualization from opt parser"
    This reverts commit 6d56e4ec73745dadff2249999d65e9fc43ed1741.

--- src/AoCUtils/Config.hs
+++ src/AoCUtils/Config.hs
@@ -7,12 +7,18 @@ import           AoCUtils.Days (Solver)
 data Config = Config {
   cfgSolvers        :: [Solver],
   cfgInputDir       :: FilePath,
-  cfgResultsDir     :: FilePath
+  cfgResultsDir     :: FilePath,
+  cfgVisualizations :: String -> IO ()
 }
 
-mkConfig :: [Solver] -> FilePath -> FilePath -> Config
-mkConfig solvers inputDir resultsDir = Config {
+mkConfig :: [Solver] -> FilePath -> FilePath -> Maybe (String -> IO ()) -> Config
+mkConfig solvers inputDir resultsDir vis = Config {
   cfgSolvers = solvers,
   cfgInputDir = inputDir,
-  cfgResultsDir = resultsDir
+  cfgResultsDir = resultsDir,
+  cfgVisualizations = vis'
   }
+  where
+    vis' = case vis of
+      Nothing    -> error "No visualizations provided"
+      Just vis'' -> vis''
--- src/AoCUtils/Interactive.hs
+++ src/AoCUtils/Interactive.hs
@@ -4,38 +4,46 @@ module AoCUtils.Interactive (
   printSolutions
 ) where
 
-import           AoCUtils.Config     (Config (cfgInputDir, cfgSolvers))
+import           AoCUtils.Config     (Config (cfgInputDir, cfgSolvers, cfgVisualizations))
 import           AoCUtils.Days       (readInput, showSolution)
 import           Control.Applicative ((<**>), (<|>))
 import           Data.Time           (diffUTCTime, getCurrentTime)
 import           Options.Applicative (Parser, ParserInfo, argument, auto,
                                       execParser, fullDesc, header, help,
-                                      helper, info, metavar, progDesc)
+                                      helper, info, metavar, progDesc, short,
+                                      strOption)
 
 aocMain :: Config -> IO ()
 aocMain cfg = do
   options <- execParser opts
   case options of
-    Textual day -> printSolutions cfg [day]
-    TextualAll  -> printSolutions cfg [1 .. length $ cfgSolvers cfg]
+    Textual day     -> printSolutions cfg [day]
+    Graphical visId -> cfgVisualizations cfg visId
+    TextualAll      -> printSolutions cfg [1 .. length $ cfgSolvers cfg]
 
 -- Opts parsing
 
-data ProgramOpts = Textual Int | TextualAll
+data ProgramOpts = Textual Int | Graphical String | TextualAll
 
 opts :: ParserInfo ProgramOpts
 opts = info (optsParser <**> helper)
   (fullDesc
-  <> progDesc "Solve a day"
+  <> progDesc "Solve a day or show visualization"
   <> header "AoC Solver" )
 
 optsParser :: Parser ProgramOpts
-optsParser = textualParser <|> allParser
+optsParser = textualParser <|> graphicalParser <|> allParser
 
 textualParser :: Parser ProgramOpts
 textualParser = Textual <$> argument auto (
   metavar "DAY"
   <> help "Day to run.")
+
+graphicalParser :: Parser ProgramOpts
+graphicalParser = Graphical <$> strOption
+  (short 'g'
+  <> metavar "VISUALIZATION"
+  <> help "Visualization to run")
 
 allParser :: Parser ProgramOpts
 allParser = pure TextualAll
